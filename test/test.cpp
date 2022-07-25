9e750227aa9aecbebc0c9eb41046f1da18aaeec2
lk1007 <kainliam0@gmail.com>
2022-07-24 22:00:29 -0400

    simplifying unit test

--- test/test.cpp
+++ test/test.cpp
@@ -4,9 +4,6 @@ TEST(exampTest, addExpr){
     Calculator calc;
     float n = calc.calcTest("1 + 1");
     ASSERT_EQ(n, 2);
-}
-TEST(exampTest, addExpr2){
-    Calculator calc;
-    float n = calc.calcTest("1 + 2*4");
-    ASSERT_EQ(n, 14);
+    n = calc.calcTest("1 - 2*4");
+    ASSERT_EQ(n, -7);
 }
