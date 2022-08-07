85aace99d68079dfd86a182600f0d9791d52f9ed
lk1007 <kainliam0@gmail.com>
2022-08-06 21:45:23 -0400

    Added more test cases, case on line 11 not working. Giving already has children error

--- test/test.cpp
+++ test/test.cpp
@@ -6,4 +6,12 @@ TEST(exampTest, addExpr){
     ASSERT_EQ(n, 2);
     n = calc.calcTest("1 - 2*4");
     ASSERT_EQ(n, -7);
+    n = calc.calcTest("");
+    ASSERT_EQ(n, 0);
+    n = calc.calcTest("4/2 + 1 - 7*8 + 48/4 - 21/7");
+    ASSERT_EQ(n, -44);
+    n = calc.calcTest("         2 + 3   ");
+    ASSERT_EQ(n, 5);
+    n = calc.calcTest("2+7                  ");
+    ASSERT_EQ(n, 9);
 }
